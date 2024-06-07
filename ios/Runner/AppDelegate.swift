0e56f9cccda14619151449f20124621c3c630190
user260066 <rrehan.3100@gmail.com>
2024-06-07 10:47:37 +0100

    Initial commit

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -3,7 +3,7 @@ import Flutter
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-  var field: UITextField?
+  var secureView: UIView?
 
   override func application(
     _ application: UIApplication,
@@ -32,40 +32,30 @@ import Flutter
   }
 
   override func applicationWillResignActive(_ application: UIApplication) {
-    field?.isSecureTextEntry = false
+    // No need to modify secureTextEntry property of UITextField here
   }
 
   override func applicationDidBecomeActive(_ application: UIApplication) {
-    field?.isSecureTextEntry = true
+    // No need to modify secureTextEntry property of UITextField here
   }
 
   private func enableScreenshotRestriction() {
     if let window = UIApplication.shared.windows.first {
-      if field == nil {
-        field = UITextField()
-        field!.translatesAutoresizingMaskIntoConstraints = false
-        field!.isSecureTextEntry = true
-        window.addSubview(field!)
-        field!.centerYAnchor.constraint(equalTo: window.centerYAnchor).isActive = true
-        field!.centerXAnchor.constraint(equalTo: window.centerXAnchor).isActive = true
-        window.layer.superlayer?.addSublayer(field!.layer)
-        if #available(iOS 17.0, *) {
-          field!.layer.sublayers?.last?.addSublayer(window.layer)
-        } else {
-          field!.layer.sublayers?.first?.addSublayer(window.layer)
-        }
-        print("Secure UITextField added to UIWindow")
+      if secureView == nil {
+        secureView = UIView(frame: window.bounds)
+        secureView!.backgroundColor = UIColor.black
+        secureView!.isUserInteractionEnabled = false
+        window.addSubview(secureView!)
+        print("Secure UIView added to UIWindow")
       }
     }
   }
 
   private func disableScreenshotRestriction() {
     if let window = UIApplication.shared.windows.first {
-      if let secureField = field {
-        secureField.removeFromSuperview()
-        field = nil
-        print("Secure UITextField removed from UIWindow")
-      }
+      secureView?.removeFromSuperview()
+      secureView = nil
+      print("Secure UIView removed from UIWindow")
     }
   }
 }
